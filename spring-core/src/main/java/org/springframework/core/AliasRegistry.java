6ecc0355b7d91dfd27a74e74864531516813ea88
hcw <897796171@qq.com>
2018-10-31 11:45:06 +0800

    翻译 AliasRegistry 用于管理别名的通用界面，作为超级接口

--- spring-core/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/AliasRegistry.java
@@ -17,6 +17,7 @@
 package org.springframework.core;
 
 /**
+ * 用于管理别名的通用界面。 作为超级接口：
  * Common interface for managing aliases. Serves as super-interface for
  * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
  *
@@ -26,33 +27,34 @@ package org.springframework.core;
 public interface AliasRegistry {
 
 	/**
+	 * 给定名称,为其注册别名：
 	 * Given a name, register an alias for it.
-	 * @param name the canonical name
-	 * @param alias the alias to be registered
-	 * @throws IllegalStateException if the alias is already in use
-	 * and may not be overridden
+	 * @param name 命名规范名称   the canonical name
+	 * @param alias 要注册的别名  the alias to be registered
+	 * @throws 如果这个别名已经被使用,产生IllegalStateException异常  IllegalStateException if the alias is already in use
+	 * 并且可能不会被覆盖  and may not be overridden
 	 */
 	void registerAlias(String name, String alias);
 
 	/**
-	 * Remove the specified alias from this registry.
-	 * @param alias the alias to remove
-	 * @throws IllegalStateException if no such alias was found
+	 * 从此注册表中删除指定的别名 Remove the specified alias from this registry.
+	 * @param alias 要删除的别名  the alias to remove
+	 * @throws 如果没有找到这样的别名,产生IllegalStateException异常  IllegalStateException if no such alias was found
 	 */
 	void removeAlias(String alias);
 
-	/**
-	 * Determine whether this given name is defines as an alias
-	 * (as opposed to the name of an actually registered component).
-	 * @param name the name to check
-	 * @return whether the given name is an alias
+	/** 
+	 * 确定此给定名称是否定义为别名  Determine whether this given name is defines as an alias
+	 * (而不是实际注册的组件的名称  as opposed to the name of an actually registered component).
+	 * @param name 要检查的名称 the name to check
+	 * @return 给定的名称是否为别名 whether the given name is an alias
 	 */
 	boolean isAlias(String name);
 
 	/**
-	 * Return the aliases for the given name, if defined.
-	 * @param name the name to check for aliases
-	 * @return the aliases, or an empty array if none
+	 * 如果已定义，则返回给定的别名 Return the aliases for the given name, if defined.
+	 * @param name 要检查的别名名称 the name to check for aliases
+	 * @return 别名，如果没有返回空数组  the aliases, or an empty array if none
 	 */
 	String[] getAliases(String name);
 
